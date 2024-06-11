dfefc7710733c378f90509fa0e865d67f77d856d
cobalt70 <giwoo.kim@gmail.com>
2024-06-11 19:54:45 +0900

    Initial Commit

---
+++ LiveWalkAndCal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveWalkAndCal
+//
+//  Created by Giwoo Kim on 6/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveWalkAndCal/LiveWalkAndCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveWalkAndCalApp.swift
+//  LiveWalkAndCal
+//
+//  Created by Giwoo Kim on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveWalkAndCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
